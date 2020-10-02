ae7cb0e2441e39cf6d588147560c0f03f89e3697
Deity <59446525+ota-deity@users.noreply.github.com>
2020-10-03 09:05:55 +1000

    Fixed giphy code
    to get api key, go to: https://developers.giphy.com/dashboard/
    change "API_KEY_HERE_PLS" to your applications API Key provided by giphy.

--- Examples/giphy.lua
+++ Examples/giphy.lua
@@ -7,30 +7,30 @@ local json = require("json")
 
 discordia.extensions()
 
-function auditLog()
-	local open = io.open("loggedServers.json", "r")
-	local parse = json.parse(open:read())
-	open:close()
+client:on("messageCreate", function(message)
+	local content = message.content
+	if content == "!giphy" then
+		local res, data = coro.request("GET", "https://api.giphy.com/v1/gifs/random?api_key=API_KEY_HERE_PLS")
+		local key,value = unpack(res)
+		local datvar = string.split((tostring(data)),'"')
 
-	local res, data = coro.request("GET", "https://api.giphy.com/v1/gifs/random?api_key= api_key_here ")
-	local data = string.split((tostring(data)),'"')
+		print("GIPHY URL :: 'https://giphy.com/media/"..datvar[10].."/giphy.gif'")
+		message:reply {
+			embed = {
+				image = { 
+					url  = "https://media4.giphy.com/media/"..tostring(datvar[10]).."/200.gif" 
+				},
 
-	print("GIPHY URL :: 'https://giphy.com/media/"..data[10].."/giphy.gif'")
-	message:reply {
-		embed = {
-			image = { 
-				url  = "https://media4.giphy.com/media/"..tostring(data[10]).."/200.gif" 
-			},
+				author = {
+					name = "Powered by Giphy",
+					icon_url = "https://cdn.discordapp.com/attachments/741617853505077320/761713715518767125/ezgif-3-9e0f1be09387.gif"
+				},
 
-			author = {
-				name = "Powered by Giphy",
-				icon_url = "https://cdn.discordapp.com/attachments/741617853505077320/761713715518767125/ezgif-3-9e0f1be09387.gif"
-			},
-
-			timestamp = discordia.Date():toISO("T","Z"),
-			color = 0x000000
+				timestamp = discordia.Date():toISO("T","Z"),
+				color = 0x000000
+			}
 		}
-	}
-end
+	end
+end)
 
-client:run("Bot "..io.open("./token.txt"):read())
+client:run("Bot "..io.open("./mtoken.txt"):read())
